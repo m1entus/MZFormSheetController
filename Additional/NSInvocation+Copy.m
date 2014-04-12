776336ad5cadb5afc596080d21f1ef7872c80fae
Michal Zaborowski <m1entus@gmail.com>
2014-04-12 17:34:59 +0200

    Fix Issue #81 -[NSInvocation copy] - only 64bit devices

--- Additional/NSInvocation+Copy.m
+++ Additional/NSInvocation+Copy.m
@@ -4,6 +4,24 @@
 //
 //  Created by Michał Zaborowski on 17.08.2013.
 //  Copyright (c) 2013 Michał Zaborowski. All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 
 #import "NSInvocation+Copy.h"
 
@@ -77,6 +95,10 @@ BOOL mz_areObjCTypesEqual(NSString *argmuentType, const char *encodingType) {
                 [invocation setArgument:&arg atIndex:index];
             } else if (mz_areObjCTypesEqual(argumentType, @encode(unsigned char))) {
                 unsigned char arg;
+                [self getArgument:&arg atIndex:index];
+                [invocation setArgument:&arg atIndex:index];
+            } else if (mz_areObjCTypesEqual(argumentType, @encode(bool))) {
+                bool arg;
                 [self getArgument:&arg atIndex:index];
                 [invocation setArgument:&arg atIndex:index];
             } else if (mz_areObjCTypesEqual(argumentType, @encode(short))) {
