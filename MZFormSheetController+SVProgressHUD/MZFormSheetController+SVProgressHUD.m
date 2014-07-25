c148277e2b7c90c495eabf8355cec0eca03c5a8f
diexee <diexee@gmail.com>
2014-07-25 18:06:58 -0300

    Update MZFormSheetController+SVProgressHUD.m
    Fix to show the SVProgressHUD when there was no last object of SVFormSheetController

--- MZFormSheetController+SVProgressHUD/MZFormSheetController+SVProgressHUD.m
+++ MZFormSheetController+SVProgressHUD/MZFormSheetController+SVProgressHUD.m
@@ -28,8 +28,10 @@
                 if(![[window class] isEqual:[MZFormSheetBackgroundWindow class]]) {
                     
                     MZFormSheetController *formSheet = [[MZFormSheetController formSheetControllersStack] lastObject];
-                    [[SVProgressHUD sharedView].overlayView removeFromSuperview];
-                    [formSheet.formSheetWindow addSubview:[SVProgressHUD sharedView].overlayView];
+                    if(formSheet){
+                        [[SVProgressHUD sharedView].overlayView removeFromSuperview];
+                        [formSheet.formSheetWindow addSubview:[SVProgressHUD sharedView].overlayView];
+                    }
                     break;
                     
                 }
